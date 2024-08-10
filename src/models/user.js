d63f6d34118b74f9ee2d4a531d567f902f2d0704
Shagil1 <shagil@spydra.app>
2024-08-11 05:13:42 +0530

    changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,59 +1,29 @@
-import mongoose from "./index.js"
+import mongoose from "./index.js";
 
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:[true,"Password is required"]
+    password: {
+      type: String,
+      required: [true, "Password is required"],
     },
+    datecreated: Date,
+    dateUpdated: Date,
+  },
+  {
+    collection: "users",
+    versionKey: false,
+    timestamps: true,
+  },
+);
+const userModel = mongoose.model("users", userSchema);
 
-    mobile: {
-        type: Number,
-        required: true,
-    },
-     work: {
-         type: String,
-        
-       trim: true
-    },
-    role:{
-        type:String,
-        default:'user'
-    },
-    add:{
-        type:String,
-        
-    },
-       status: {
-            type: String,
-            required:[true,"Status is Required"],
-            enum: ['Active' , 'InActive'],
-            default: 'Active',
-           
-        },
-    
-    desc: {
-        type: String,
-        required: true,
-    },
-
-    datecreated:Date,
-    dateUpdated:Date
-},{
-    collection:'users',
-    versionKey:false,
-    timestamps:true
-})
-const userModel = mongoose.model('users',userSchema)
-
-export default userModel
+export default userModel;
